60c67a27ee6d2ad02fd20ad552a7514d0fd4a092
Ryan Mack <rmack005@gmail.com>
2014-04-10 17:51:09 -0400

    Fixed bug in validate.js (breaking change).  Needed to alter how the validation functions are applied.  Previously, the property value was passed for this, but that doesn't work as intended for non-object values.  Now the object being validated is used for this and the property value is the first parameter.

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 js-svu
 ======
 
-A simple object validation utility for javascript
+A simple object validation utility for javascript inspired by [`Fluent Validation`](http://fluentvalidation.codeplex.com/) 
+and [`Mocha`](http://fluentvalidation.codeplex.com/).  Combine with your favorite assertion library (I like [Chai](http://chaijs.com/)).
 
 ```js
 var validation = require("js-svu");
 
 var validator = validation.create({
-    field1: function() {expect(this).to.equal(1);},
-    field2: function() {expect(this).to.equal("test");}
+    field1: function(it) {expect(it).to.equal(1);},
+    field2: function(it) {expect(it).to.equal("test");}
 });
 
 var obj = {
@@ -18,3 +19,61 @@ var obj = {
 
 var result = validator(obj);
 ```
+
+__Warning__: this library is still experimental and the API unstable.
+
+## Installation
+
+Using [`npm`](http://npmjs.org/):
+
+```bash
+npm install js-svu
+```
+
+## Quick Start
+
+First, define a validator.
+
+```js
+var validator = validation.create({
+    field1: function(it) {expect(it).to.equal(1);},
+    field2: function(it) {expect(it).to.equal("test");}
+});
+```
+
+The above validator ensures that every object it validates has a property 
+named "field1" one that's equal to 1, and a property named "field2"
+that's equal to "test".
+
+Next, use the validator to validate an object.
+
+```js
+var obj = {
+    field1: 1,
+    field2: "test1",
+};
+
+var result = validator(obj);
+var isValid = validation.isValid(result)
+```
+
+The object above should fail validation (field2 is equal to "test1", not "test").
+Examine the result to determine if the object passed validation, and if not,
+why.  For each property in the object being validated a property with the same name
+is added to the result object, but instead of containing a copy of the value
+it contains an error message.  The presense of a property in the result object
+indicates a problem with the associated property in the validated object.  If a
+result object contains no properties, validation succeeded.
+
+
+## Features
+
+Not written yet.
+
+## Philosophy
+
+Not written yet.
+
+## Examples
+
+Not written yet.
--- test/unitTests.js
+++ test/unitTests.js
@@ -44,8 +44,8 @@ describe('CollectionValidator',function() {
 describe('Validator Method',function() { 
     it('should return error(s) if the object is not valid.', function(){
         var validator = validation.create({
-            field1: function() {expect(this).to.equal(1);},
-            field2: function() {expect(this).to.equal("test");}
+            field1: function(it) {expect(it).to.equal(1);},
+            field2: function(it) {expect(it).to.equal("test");}
         });
 
         var obj = {
@@ -55,20 +55,20 @@ describe('Validator Method',function() {
 
         var result = validator(obj);
 
-        expect(validation.isResultValid(result)).to.equal(false);
+        expect(validation.isValid(result)).to.equal(false);
         expect(result).to.have.property('field2');
         expect(result).to.not.have.property('field1');
     }); 
 
     it('should support nested validators.', function(){
         var validator1 = validation.create({
-            inner1: function() {expect(this).to.equal("test");}
+            inner1: function(it) {expect(it).to.equal("test");}
         });
 
         var validator2 = validation.create({
-            field1: function() {expect(this).to.equal(1);},
-            field2: function() {expect(this).to.equal("test");},
-            field3: function() {return validator1(this);}
+            field1: function(it) {expect(it).to.equal(1);},
+            field2: function(it) {expect(it).to.equal("test");},
+            field3: function(it) {return validator1(it);}
         });
 
         var obj = {
@@ -81,7 +81,7 @@ describe('Validator Method',function() {
 
         var result = validator2(obj);
 
-        expect(validation.isResultValid(result)).to.equal(false);
+        expect(validation.isValid(result)).to.equal(false);
         expect(result).to.have.property('field2');
         expect(result).to.have.property('field3');
         expect(result.field3).to.have.property('inner1');
@@ -90,8 +90,8 @@ describe('Validator Method',function() {
 
     it('should not return error(s) if the object is valid.', function(){
         var validator = validation.create({
-            field1: function() {expect(this).to.equal(1);},
-            field2: function() {expect(this).to.equal("test");}
+            field1: function(it) {expect(it).to.equal(1);},
+            field2: function(it) {expect(it).to.equal("test");}
         });
 
         var obj = {
@@ -101,7 +101,7 @@ describe('Validator Method',function() {
 
         var result = validator(obj);
 
-        expect(validation.isResultValid(result)).to.equal(true);
+        expect(validation.isValid(result)).to.equal(true);
         expect(result).to.not.have.property('field1');
         expect(result).to.not.have.property('field2');
     }); 
@@ -111,9 +111,9 @@ describe('Validator Method',function() {
 describe('CollectionValidator Method',function() { 
     it('should return error(s) if the object(s) are not valid.', function(){
         var myObjectValidator = validation.create({
-            id: function(obj) {expect(this).to.be.a('number');},
-            field1: function(obj) {expect(this).to.be.a('number');},
-            field2: function(obj) {expect(this).to.be.a("string");}
+            id: function(it) {expect(it).to.be.a('number');},
+            field1: function(it) {expect(it).to.be.a('number');},
+            field2: function(it) {expect(it).to.be.a("string");}
         });
 
         var myObjectCollectionValidator = validation.create(myObjectValidator);
@@ -126,7 +126,7 @@ describe('CollectionValidator Method',function() {
 
         var result = myObjectCollectionValidator(array);
 
-        expect(validation.isResultValid(result)).to.equal(false);
+        expect(validation.isValid(result)).to.equal(false);
 
         //  can't rely on result.length because the array may be sparse
         expect(Object.keys(result)).to.have.length(1);
@@ -136,13 +136,13 @@ describe('CollectionValidator Method',function() {
 describe('getErrorCollection',function() { 
     it('should return a flat list of errors.', function(){
         var validator1 = validation.create({
-            inner1: function() {expect(this).to.equal("test");}
+            inner1: function(it) {expect(it).to.equal("test");}
         });
 
         var validator2 = validation.create({
-            field1: function() {expect(this).to.equal(1);},
-            field2: function() {expect(this).to.equal("test");},
-            field3: function() {return validator1(this);}
+            field1: function(it) {expect(it).to.equal(1);},
+            field2: function(it) {expect(it).to.equal("test");},
+            field3: function(it) {return validator1(it);}
         });
 
         var obj = {
@@ -156,7 +156,7 @@ describe('getErrorCollection',function() {
         var result = validator2(obj);
         var errorCollection = validation.getErrorCollection(result);
 
-        expect(validation.isResultValid(result)).to.equal(false);
+        expect(validation.isValid(result)).to.equal(false);
         expect(Object.keys(errorCollection)).to.have.length(2);
     }); 
 });
